3c81f60d5ae68f09e0fe3eded45760ef09166722
Moroz314 <vladmorozov2020@mail.ru>
2022-06-14 20:55:56 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Class8d.sayt" 
---
+++ main.js
@@ -0,0 +1,5 @@
+let card = document.querySelector('.card')
+let card_toreg = document.querySelector('.toreg')
+card_toreg.onclick = function(){
+    card.classList.card_toreg('active')
+}
